6fff74f20a40ae6e06df5ee93c014f61f4b5c6d8
kellara <ammon502@byui.edu>
2023-12-11 22:56:36 -0700

    Foundation 4 complete

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities = new List<Activity>();
+
+        Running running = new Running("03 Nov 2022", 30, 3.0);
+        Bicycle cycling = new Bicycle("03 Nov 2022", 10, 10.0);
+        Swimming swimming = new Swimming("03 Nov 2022", 20, 200);
+
+        // System.Console.WriteLine(running.GetSummary());
+        // System.Console.WriteLine(cycling.GetSummary());
+        // System.Console.WriteLine(swimming.GetSummary());
+
+        activities.Add(running);
+        activities.Add(cycling);
+        activities.Add(swimming);
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
